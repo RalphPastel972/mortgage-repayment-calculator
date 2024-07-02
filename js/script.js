c1568232be8cbdc39442e3d7eb630424eca2deaf
Ralph Pastel <ralph.pastel@gmail.com>
2024-07-03 00:07:31 +0200

    V0.1 finished
    A LOT of polish is still necessary

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let inputRadioRepayment = document.getElementById("input_radio_repayment");
 let inputRadioInterestOnly = document.getElementById(
   "input_radio_interest_only"
 );
+let canWeProceedToTheResultPage = false;
 
 const clearAllFieldsButton = document.getElementById("clear_all_fields_button");
 clearAllFieldsButton.addEventListener("click", () => {
@@ -17,11 +18,11 @@ clearAllFieldsButton.addEventListener("click", () => {
 
 const calculateRepaymentButton = document.getElementById("submit_button");
 calculateRepaymentButton.addEventListener("click", () => {
-  checkIfEachFieldIsFilled();
+  triggerWarningsInCaseOfEmptyFields();
   calculationEngine();
 });
 
-function checkIfEachFieldIsFilled() {
+function triggerWarningsInCaseOfEmptyFields() {
   if (inputMortgageAmount.value == "") {
     const warningMissingMortgageAmountPart1 =
       document.getElementById("warning_field_1");
@@ -124,30 +125,91 @@ function checkIfEachFieldIsFilled() {
   }
 }
 
-function calculationEngine() {
-    const loanAmount = inputMortgageAmount.value;
-    const numberOfPayments = inputMortgageTerm.value * 12;
-    const monthlyInterestRate = (inputInterestRate.value / 100) / 12;
-    let finalResults = {};
-    
-    let monthlyMortgagePayment = (loanAmount * monthlyInterestRate) / (1 - (1 / Math.pow((1 + monthlyInterestRate), numberOfPayments) ))
-
-    let totalAmountToRepay = monthlyMortgagePayment * numberOfPayments;
-
-    let monthlyInterestPayment = monthlyMortgagePayment - (loanAmount / numberOfPayments);
-
-    let costOfLoan = totalAmountToRepay - loanAmount;
-
-    // Let's put the set of results in an object for convenience.
-    // Let's also make sure we save only values rounded to 2 digits
-    finalResults.monthlyMortgagePayment = `£${Number.parseFloat(monthlyMortgagePayment).toFixed(2)}`;
-    finalResults.totalAmountToRepay = `£${Number.parseFloat(totalAmountToRepay).toFixed(2)}`;
-    finalResults.monthlyInterestPayment = `£${Number.parseFloat(monthlyInterestPayment).toFixed(2)}`;
-    finalResults.costOfLoan = `£${Number.parseFloat(costOfLoan).toFixed(2)}`;
-    console.log(finalResults);
-    return finalResults;
-
+function checkIfAllFieldsAreFilled() {
+  if (
+    (inputMortgageAmount.value != "" &&
+      inputMortgageTerm.value != "" &&
+      inputInterestRate.value != "" &&
+      inputRadioRepayment.checked == true) ||
+    inputRadioInterestOnly.checked == true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+function calculationEngine() {
+  const loanAmount = inputMortgageAmount.value;
+  const numberOfPayments = inputMortgageTerm.value * 12;
+  const monthlyInterestRate = inputInterestRate.value / 100 / 12;
+  let finalResults = {};
 
+  let monthlyMortgagePayment =
+    (loanAmount * monthlyInterestRate) /
+    (1 - 1 / Math.pow(1 + monthlyInterestRate, numberOfPayments));
+  let totalAmountToRepay = monthlyMortgagePayment * numberOfPayments;
+  let monthlyInterestPayment =
+    monthlyMortgagePayment - loanAmount / numberOfPayments;
+  let costOfLoan = totalAmountToRepay - loanAmount;
 
+  finalResults.monthlyMortgagePayment = monthlyMortgagePayment;
+  finalResults.totalAmountToRepay = totalAmountToRepay;
+  finalResults.monthlyInterestPayment = monthlyInterestPayment;
+  finalResults.costOfLoan = costOfLoan;
+  console.log(finalResults);
+  formatingResults(finalResults);
+  return finalResults;
+}
+
+function formatingResults(resultsInObject) {
+  let finalResults = {};
+
+  // Rounding the results to 2 decimal digits + forcing the result into numbers instead of strings
+  finalResults.monthlyMortgagePayment = Number(
+    Number.parseFloat(resultsInObject.monthlyMortgagePayment).toFixed(2)
+  );
+  finalResults.totalAmountToRepay = Number(
+    Number.parseFloat(resultsInObject.totalAmountToRepay).toFixed(2)
+  );
+  finalResults.monthlyInterestPayment = Number(
+    Number.parseFloat(resultsInObject.monthlyInterestPayment).toFixed(2)
+  );
+  finalResults.costOfLoan = Number(
+    Number.parseFloat(resultsInObject.costOfLoan).toFixed(2)
+  );
+  console.log(finalResults);
+
+  // Formating the values according to the user's locale (language and region) + adding "£" at the beginning.
+  finalResults.monthlyMortgagePayment = `£${finalResults.monthlyMortgagePayment.toLocaleString()}`;
+  finalResults.totalAmountToRepay = `£${finalResults.totalAmountToRepay.toLocaleString()}`;
+  finalResults.monthlyInterestPayment = `£${finalResults.monthlyInterestPayment.toLocaleString()}`;
+  finalResults.costOfLoan = `£${finalResults.costOfLoan.toLocaleString()}`;
+  console.log(finalResults);
+  displayResultsInApp(finalResults);
+  return finalResults;
+}
+
+function displayResultsInApp(resultsInObject) {
+  if (checkIfAllFieldsAreFilled() == true) {
+    let monthlyMortgagePayment = document.getElementById("final_result_1");
+    let totalAmountToRepay = document.getElementById("final_result_2");
+
+    if (inputRadioRepayment.checked === true) {
+      monthlyMortgagePayment.innerText = resultsInObject.monthlyMortgagePayment;
+      totalAmountToRepay.innerText = resultsInObject.totalAmountToRepay;
+    } else {
+      monthlyMortgagePayment.innerText = resultsInObject.monthlyInterestPayment;
+      totalAmountToRepay.innerText = resultsInObject.costOfLoan;
+    }
+
+    const preResultPannel = document.getElementById("empty_results_area");
+    const displayOfResults = document.getElementById("display_of_results");
+
+    preResultPannel.classList.remove("pannel_is_visible");
+    displayOfResults.classList.remove("pannel_is_hidden");
+
+    preResultPannel.classList.add("pannel_is_hidden");
+    displayOfResults.classList.add("pannel_is_visible");
+  }
+}
